8d00089e5a24d97f1e06dfa9c2b8e3c16c7416e0
Shruti Tiwari <sh03shruti@gmail.com>
2023-02-04 23:26:10 +0530

    aftertaste attribute added

--- test.py
+++ test.py
@@ -18,14 +18,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def quality():
-    print(portnumber)
     data = request.args.get('my_dict')
     stringdict = format(json.dumps(data))
     my_dataDict = json.loads(stringdict)
     myData = json.loads(my_dataDict)
 
-    coffee = pd.read_csv("merged_data_cleaned.csv")
-    coffee_data = coffee[["Aroma", "Flavor", "Acidity", "Body", "Balance", "Uniformity", "Clean.Cup", "Sweetness", "Cupper.Points",
+    coffee = pd.read_csv("merged_data_cleaned_new.csv")
+    coffee_data = coffee[["Aroma", "Flavor", "Aftertaste", "Acidity", "Body", "Balance", "Uniformity", "Clean.Cup", "Sweetness", "Cupper.Points",
                           "Moisture", "Quakers", "Category.One.Defects", "Category.Two.Defects", "altitude_mean_meters", "Total.Cup.Points"]]
 
     x = coffee_data["altitude_mean_meters"].mean()
@@ -42,6 +41,7 @@ def quality():
     a = {
         "Aroma": float(myData['aroma']),
         "Flavor": float(myData['Flavor']),
+        "Aftertaste": float(myData['Aftertaste']),
         "Acidity": float(myData['Acidity']),
         "Body": float(myData['Body']),
         "Balance": float(myData['Balance']),
@@ -56,7 +56,7 @@ def quality():
         "altitude_mean_meters": float(myData['altitude_mean_meters'])
     }
     # A = np.array([[a['Aroma'], 8.83, 8.75, 8.5, 8.42, 10.0, 10.0,10.0, 8.75, 0.12, 0.0, 0, 0, 2075.0]])
-    A = np.array([[a['Aroma'], a["Flavor"], a["Acidity"], a["Body"], a["Balance"], a["Uniformity"], a["Clean.Cup"], a["Sweetness"],
+    A = np.array([[a['Aroma'], a["Flavor"], a['Aftertaste'], a["Acidity"], a["Body"], a["Balance"], a["Uniformity"], a["Clean.Cup"], a["Sweetness"],
                  a["Cupper.Points"], a["Moisture"], a["Quakers"], a["Category.One.Defects"], a["Category.Two.Defects"], a["altitude_mean_meters"]]])
     single_pred = model.predict(A)
     print(single_pred)
@@ -70,4 +70,4 @@ if __name__ == '__main__':
 
 
 # http://localhost:5000?my_dict={"aroma":8.67,"Flavor":8.83}
-# http://localhost:5000?my_dict={"aroma":8.67,"Flavor":8.83,"Acidity":8.75,"Body":8.5,"Balance":8.42,"Uniformity":10.0,"Clean.Cup":10.0,"Sweetness":10.0,"Cupper.Points":8.75,"Moisture":0.12,"Quakers":0.0,"Category.One.Defects":0.0,"Category.Two.Defects":0,"altitude_mean_meters":2075.0}
+# http://localhost:5000?my_dict={"aroma":8.67,"Flavor":8.83,"Aftertaste":8.6,"Acidity":8.75,"Body":8.5,"Balance":8.42,"Uniformity":10.0,"Clean.Cup":10.0,"Sweetness":10.0,"Cupper.Points":8.75,"Moisture":0.12,"Quakers":0.0,"Category.One.Defects":0.0,"Category.Two.Defects":0,"altitude_mean_meters":2075.0}
