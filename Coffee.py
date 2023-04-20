5f7507ecd9803f33dfac9ed749a1081e4b071f8f
Shruti Tiwari <sh03shruti@gmail.com>
2023-04-20 23:35:46 +0530

    test3

--- Coffee.py
+++ Coffee.py
@@ -62,7 +62,7 @@ def quality():
     print(single_pred)
     # return render_template('/index.html', quality=single_pred[0])
     output = {'quality': "{}".format(single_pred[0])}
-    return output
+    return jsonify(output)
 
 
 if __name__ == '__main__':
