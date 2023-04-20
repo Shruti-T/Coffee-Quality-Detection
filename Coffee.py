ae8d45ca27b0e10d5252faad660ed7e238459278
Shruti Tiwari <sh03shruti@gmail.com>
2023-04-20 22:54:47 +0530

    brackets removed in return

--- Coffee.py
+++ Coffee.py
@@ -18,6 +18,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def quality():
+    request.json
     data = request.args.get('my_dict')
     stringdict = format(json.dumps(data))
     my_dataDict = json.loads(stringdict)
@@ -61,7 +62,7 @@ def quality():
     single_pred = model.predict(A)
     print(single_pred)
     # return render_template('/index.html', quality=single_pred[0])
-    output = {'quality': "{}".format(single_pred[0])}
+    output = {'quality': single_pred[0]}
     return jsonify(output)
 
 
