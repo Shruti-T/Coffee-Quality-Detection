6fbc32db022bb0f08ecc345482b0b31dc070459c
Shruti Tiwari <sh03shruti@gmail.com>
2023-12-18 09:56:10 +0530

    commented out unused library

--- Coffee.py
+++ Coffee.py
@@ -1,14 +1,14 @@
-from sklearn.metrics import r2_score
+# from sklearn.metrics import r2_score
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
-from sklearn import metrics
+# from sklearn import metrics
 from flask import *
 import json
-from jinja2 import Environment, BaseLoader
+# from jinja2 import Environment, BaseLoader
 import os
 
 portnumber = int(os.environ.get('PORT', 5000))
